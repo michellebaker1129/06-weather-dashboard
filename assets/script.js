b0b73b729997dba90d064d9f2297bc3a3bda9960
Michelle <michellebaker1129@gmail.com>
2023-04-11 21:45:54 -0700

    rest the clock to when things worked, then bug fix

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 var cityFormEl = document.getElementById("city-form");
-var currentDate = dayjs()
-console.log(currentDate)
+var currentDate = dayjs();
 var key = "ee58e3e024677f636e675b8a11b6a05c";
+
+// create a function to fetch all data for a city
+// hit all apis needed
+// save that data to local storage
+
 function getLatLon(city) {
   console.log(city);
   fetch(
@@ -27,31 +31,36 @@ function getWeather(lat, lon) {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      var card = $("<div>").addClass("card");
-      var cardTitle = $("<h2>").addClass("card-title").text(data.name);
-      var cardBody = $("<div>").addClass("card-body");
-      var tempEl = $("<h4>")
-        .addClass("card-text")
-        .text("temperature: " + Math.round(data.main.temp) + " °F");
-      var icon = $("<img>").attr(
-        "src",
-        `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-      );
-      var humidityEl = $("<h4>")
-        .addClass("card-text")
-        .text("humidity: " + data.main.humidity + "%");
-      var windSpeedEl = $("<h4>")
-        .addClass("card-text")
-        .text("wind speed: " + data.wind.speed + " mph");
-      $("#weather").append(
-        card.append(
-          cardTitle.append(icon),
-          cardBody.append(tempEl, humidityEl, windSpeedEl)
-        )
-      );
+      renderWeather(data);
     });
   get5DayForecast(lat, lon);
+}
+
+// create a function to render weather information on the page
+function renderWeather(data) {
+  var card = $("<div>").addClass("card");
+  var cardTitle = $("<h2>").addClass("card-title").text(data.name);
+  var cardBody = $("<div>").addClass("card-body");
+  var tempEl = $("<h4>")
+    .addClass("card-text")
+    .text("temperature: " + Math.round(data.main.temp) + " °F");
+  var icon = $("<img>").attr(
+    "src",
+    `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  );
+  var humidityEl = $("<h4>")
+    .addClass("card-text")
+    .text("humidity: " + data.main.humidity + "%");
+  var windSpeedEl = $("<h4>")
+    .addClass("card-text")
+    .text("wind speed: " + data.wind.speed + " mph");
+
+  $("#weather").html(
+    card.append(
+      cardTitle.append(icon),
+      cardBody.append(tempEl, humidityEl, windSpeedEl)
+    )
+  );
 }
 
 function get5DayForecast(lat, lon) {
@@ -62,38 +71,54 @@ function get5DayForecast(lat, lon) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      for (var i = 0; i < data.list.length; i++) {
-        var testTime = data.list[i].dt_txt.split(" ").pop();
-        var targetTime = "00:00:00";
-        if (testTime === targetTime) {
-        
-          var forecast = data.list[i];
-          var dateDisplay = dayjs.unix(forecast.dt).format("ddd");
-          var card = $("<div>").addClass("card").attr("style","width:fit-content");
-          var cardTitle = $("<h2>").addClass("card-title").text(dateDisplay);
-          var cardBody = $("<div>").addClass("card-body");
-          var tempEl = $("<h4>")
-            .addClass("card-text")
-            .text("temperature: " + Math.round(forecast.main.temp) + " °F");
-          var icon = $("<img>").attr(
-            "src",
-            `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
-          );
-          var humidityEl = $("<h4>")
-            .addClass("card-text")
-            .text("humidity: " + forecast.main.humidity + "%");
-          var windSpeedEl = $("<h4>")
-            .addClass("card-text")
-            .text("wind speed: " + forecast.wind.speed + " mph");
-            $("#forecast").append(card.append(cardTitle.append(icon),
-            cardBody.append(tempEl, humidityEl, windSpeedEl)))
-        }
-      }
+      render5DayForecast(data);
     });
+}
+
+// create a function to render 5 day forcast on the page
+function render5DayForecast(data) {
+  const newCards = [];
+
+  for (var i = 0; i < data.list.length; i++) {
+    var testTime = data.list[i].dt_txt.split(" ").pop();
+    var targetTime = "00:00:00";
+    
+    if (testTime === targetTime) {
+      var forecast = data.list[i];
+      var dateDisplay = dayjs.unix(forecast.dt).format("ddd");
+      var card = $("<div>")
+        .addClass("card")
+      var cardTitle = $("<h2>").addClass("card-title").text(dateDisplay);
+      var cardBody = $("<div>").addClass("card-body");
+      var tempEl = $("<h4>")
+        .addClass("card-text")
+        .text("temperature: " + Math.round(forecast.main.temp) + " °F");
+      var icon = $("<img>").attr(
+        "src",
+        `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
+      );
+      var humidityEl = $("<h4>")
+        .addClass("card-text")
+        .text("humidity: " + forecast.main.humidity + "%");
+      var windSpeedEl = $("<h4>")
+        .addClass("card-text")
+        .text("wind speed: " + forecast.wind.speed + " mph");
+
+      newCards.push(
+        card.append(
+          cardTitle.append(icon),
+          cardBody.append(tempEl, humidityEl, windSpeedEl)
+        )
+      )
+    }
+  }
+
+  $("#forecast").html(newCards);
 }
 
 cityFormEl.addEventListener("submit", function (event) {
   event.preventDefault();
   var cityName = document.getElementById("city").value;
   getLatLon(cityName);
+  // save city name to local storage
 });
