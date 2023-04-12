3ab36a650c968116c727c246b098db63ad416819
Michelle <michellebaker1129@gmail.com>
2023-04-12 11:07:24 -0700

    fix https link

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ renderCityButtons(cities);
 function fetchDataFromApi(city) {
   // fetch from weather geo api with city name
   fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
     "&limit=5&appid=" +
     key
